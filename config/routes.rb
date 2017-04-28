6991297fecae66e3b34f3ea771784a6c46b70879
Paul <paucho1437@yahoo.com>
2017-04-28 11:12:44 -0500

    api controller - C

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+  get 'events/create'
+  end
+
+  get 'events/create'
+
   resources :registered_applications, except: [:edit, :update]
   devise_for :users
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
   get 'about' => 'welcome#about'
   root 'welcome#index'
   end
